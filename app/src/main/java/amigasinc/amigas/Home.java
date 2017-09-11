1b5b6710110855b7f7ef655977f5410344262dcc
DxExWxExY <dxexwxexy@gmail.com>
2017-09-11 00:54:25 -0600

    Updated icon on the bottom bar

--- app/src/main/java/amigasinc/amigas/Home.java
+++ app/src/main/java/amigasinc/amigas/Home.java
@@ -1,30 +1,32 @@
 package amigasinc.amigas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private TextView contentTest;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
+        switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    contentTest.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_map);
+                case R.id.navigation_map:
+                    contentTest.setText(R.string.title_map);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_account);
+                case R.id.navigation_account:
+                    contentTest.setText(R.string.title_account);
                     return true;
             }
             return false;
@@ -37,8 +39,10 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        contentTest = (TextView) findViewById(R.id.viewTest);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
